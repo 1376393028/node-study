64304fd085f57a7f4e150fd80f200bac4d96bf08
zhazhahui <1376393028@qq.com>
2022-12-19 21:15:08 +0800

    fs.readFile

---
+++ src/fs.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+//参数1 读取文件的路径 
+//参数2（可选） 读取文件时采用的编码格式
+//参数3 读取文件的回调函数，返回读取失败或成功结果
+fs.readFile('./src/index.js', 'utf8', function(err, dataStr) {
+    console.log(err); //读取成功 err为null, 读取失败 dataStr为undefined
+    console.log('----');
+    console.log(dataStr);
+})
