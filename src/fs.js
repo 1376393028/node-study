459fb24b718d75df09dabbf3080e222b46bea512
zhazhahui <1376393028@qq.com>
2022-12-19 21:36:09 +0800

    判断读取成功

--- src/fs.js
+++ src/fs.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 //参数2（可选） 读取文件时采用的编码格式
 //参数3 读取文件的回调函数，返回读取失败或成功结果
 fs.readFile('./src/index.js', 'utf8', function(err, dataStr) {
-    console.log(err); //读取成功 err为null, 读取失败 dataStr为undefined
-    console.log('----');
-    console.log(dataStr);
+    //读取成功 err为null, 读取失败 dataStr为undefined
+    if(!dataStr) {
+        return console.log('读取失败', err.message);
+    } 
+    console.log('读取成功');
 })
