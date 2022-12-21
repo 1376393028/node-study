83c0e6d56a3d30cf55d0c97d3bed2521c5d54f9a
zhazhahui <1376393028@qq.com>
2022-12-21 11:26:38 +0800

    时钟案例处理HTML

--- src/clockTest/index.js
+++ src/clockTest/index.js
@@ -14,16 +14,29 @@ function resolveCSS(htmlStr) {
 }
 
 function resolveJS(htmlStr) {
-    const r1 = regScript.exec(htmlStr);
-    const newJS = r1[0].replace('<script>', '').replace('</script>', '');
+    const r2 = regScript.exec(htmlStr);
+    const newJS = r2[0].replace('<script>', '').replace('</script>', '');
     fs.writeFile(path.join(__dirname, './clock/clock.js'), newJS, 'utf8', function(err) {
         if(err === null) return console.log('写入js成功');
         console.log('写入js失败', err.message);
     });
 }
 
-fs.readFile(path.join(__dirname, './clock.html'), 'utf8', function(err, dataStr) {
+function resolveHTML(htmlStr) {
+    let newHTML = htmlStr.replace(regStyle, "<link rel='stylesheet' href='./clock.css'></link>").replace(regScript, "<script src='./clock.js'></script>");
+    fs.writeFile(path.join(__dirname, './clock/clock.html'), newHTML, 'utf8', function(err) {
+        if(err === null) return console.log('写入html成功');
+        console.log('写入html失败', err.message);
+    })
+}
+
+function resolve(htmlStr) {
+    resolveCSS(htmlStr);
+    resolveJS(htmlStr);
+    resolveHTML(htmlStr);
+}
+
+fs.readFile(path.join(__dirname, './index.html'), 'utf8', function(err, dataStr) {
     if(!dataStr) return console.log('读取HTML失败', err.message);
-    resolveCSS(dataStr);
-    resolveJS(dataStr);
+    resolve(dataStr);
 });
