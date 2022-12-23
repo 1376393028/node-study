d256a3df7f9af8a31b1e2f3c68a73cf76817e998
zhazhahui <1376393028@qq.com>
2022-12-23 18:34:27 +0800

    express.static

---
+++ express.static.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// 托管静态资源
+// http://127.0.0.1/clock.html
+app.use(express.static('./src/clock-test/clock'));
+// 添加前缀
+// http://127.0.0.1/public/index.html
+app.use('/public',express.static('./src/clock-test'));
+
+app.listen(80, () => {
+    console.log('express server runing at 127.0.0.1');
+})
