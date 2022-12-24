3545fd367c25c4f4c186a8a76ae56b986d1764b7
zhazhahui <1376393028@qq.com>
2022-12-24 16:07:19 +0800

    错误中间件

--- middleware.js
+++ middleware.js
@@ -46,6 +46,17 @@ app.get('/user', [mw1, mw2], (req, res) => {
     res.send('user page ' + req.startTime);
 })
 
+app.get('/err', (req, res) => {
+    throw new Error('服务器发生错误！');
+    res.send('home page');
+})
+
+// 错误级中间件必须在路由之后 
+app.use((err, req, res, next) => {
+    console.log('发生了错误', err.message);
+    res.send('error ' + err.message);
+})
+
 app.listen(80, () => {
     console.log('http://127.0.0.1');
 })
