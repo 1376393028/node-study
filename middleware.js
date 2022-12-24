be46e7c68f212089fd1d46e9875f261653e6d0b9
zhazhahui <1376393028@qq.com>
2022-12-24 15:34:24 +0800

    局部中间件

--- middleware.js
+++ middleware.js
@@ -21,11 +21,27 @@ app.use((req, res, next) => {
     next();
 })
 
-app.get('/', (req, res) => {
+const mw1 = (req, res, next) => {
+    console.log('调用了局部中间件mw1');
+    next();
+}
+
+const mw2 = (req, res, next) => {
+    console.log('调用了局部中间件mw2');
+    next();
+}
+
+// 中间件一定要在路由之前注册
+// 客户端发送过来的请求，可以连续调用多个中间件进行处理
+// 执行完中间件，不要忘记调用next()函数
+// next()后不要再写额外的代码
+// 多个中间件之间，共享req对象和res对象
+
+app.get('/', mw1, mw2, (req, res) => {
     res.send('home page ' + req.startTime);
 })
 
-app.get('/user', (req, res) => {
+app.get('/user', [mw1, mw2], (req, res) => {
     console.log('调用 user 路由');
     res.send('user page ' + req.startTime);
 })
