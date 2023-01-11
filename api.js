72ee1aece5049bed7095ea9b0ecf4d371253b6df
zhazhahui <1376393028@qq.com>
2023-01-11 19:21:29 +0800

    jsonp

--- api.js
+++ api.js
@@ -3,6 +3,14 @@ const app = express();
 const router = require('./api-router');
 const cors = require('cors');
 
+// jsonp 接口在 cors 前
+app.get('/api/jsonp', (req, res) => {
+    const funcName = req.query.callback;
+    const data = {name: 'hui', age: 24};
+    const scriptStr = `${funcName}(${JSON.stringify(data)})`;
+    res.send(scriptStr);
+})
+
 app.use(cors());
 
 app.use(express.urlencoded({extended: false}));
